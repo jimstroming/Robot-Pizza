325b0aca79246bc16e5c57640955c47281748746
Jim Stroming <jimstroming@gmail.com>
2017-02-23 21:22:51 -0600

    push the problem description

--- DrowningSwimmer.java
+++ DrowningSwimmer.java
@@ -1,1 +1,22 @@
+/* 
+
+Can you save the drowning Swimmer?
+
+From 
+https://fivethirtyeight.com/features/can-you-save-the-drowning-swimmer/
+
+You are a lifeguard standing on the beach, right at the edge of the water, 
+and gazing out over the ocean. You see someone drowning 100 meters to the 
+right of you and 100 meters away from shore. You can run 100 meters in 15 
+seconds and swim 100 meters in 75 seconds. (The beach drops off steeply, 
+meaning that you can not run in the water.) What is the fastest you can get 
+to the victim?
+
+*/
  
+/*
+
+
+
+
+*/
