da557f26a26ccb757d6f79e721b3c481319f5143
Jim Stroming <jimstroming@gmail.com>
2017-02-25 15:27:23 -0600

    define the birthday cake problem

---
+++ BirthdayCake/BirthdayCake.java
@@ -0,0 +1,8 @@
+/*
+It is your 30th birthday (congrats, by the way), and your friends bought you a 
+cake with 30 candles on it. You make a wish and try to blow them out. Every time
+you blow, you blow out a random number of candles between one and the number 
+that remain, including one and that other number. How many times do you blow 
+before all the candles are extinguished, on average?
+
+*/
