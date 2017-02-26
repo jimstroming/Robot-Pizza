3dd497d9e3cf7897e7e371895c70851d46a5d2eb
Jim Stroming <jimstroming@gmail.com>
2017-02-26 09:51:32 -0600

    outline the race conditions we need to resolve to make the class thread safe

--- BirthdayCake/BirthdayCake.java
+++ BirthdayCake/BirthdayCake.java
@@ -18,21 +18,26 @@ public final class BirthdayCake {
     
     public BirthdayCake(int candlesoncake){
         maxcandles = candlesoncake;
-        numbercandles = candlesoncake;
+        numbercandles = candlesoncake;      // invariant: numbercandles >= 0
         numberblows = 0;
     }
     
     public int blowcandles(int numbercandlesblown){
-        numbercandles -= numbercandlesblown;
-        numberblows += 1;
+        numbercandles -= numbercandlesblown; // potential race
+        numberblows += 1;  // need the decrement in number of candles
+                           // to be atomic with the increment in numberblows
         System.out.println(numbercandles);
         return numbercandles;
     }
     
     public void lightcake(){
-        numbercandles = maxcandles;
-        numberblows = 0;
+        numbercandles = maxcandles;  // potential race
+        numberblows = 0;    // need the resetting of number of candles to be
+                            // atomic with the resetting of numberofblows
     }
+    
+    // also, can't have one thread lighting the cake while another thread
+    // is blowing the candles.
     
     public int getnumberblows(){
         return numberblows;
@@ -41,19 +46,21 @@ public final class BirthdayCake {
     public void blowrandomcandles(){
         Random rand = new Random();
         int randomnumbercandles =  rand.nextInt(numbercandles)+1;
-        blowcandles(randomnumbercandles);
+        blowcandles(randomnumbercandles);  // as long as blowcandles is locked,
+                                           // we will be good.
     }
     
     public int blowoutcake(){
         lightcake();
-        while (numbercandles > 0){
-            blowrandomcandles();
+        while (numbercandles > 0){      // potential races.
+            blowrandomcandles();        // numbercandles could change before we 
+                                        // call blowrandomcandles
         }
         return numberblows;
     }
     
     public static void main(String[] args){
-        BirthdayCake cake = new BirthdayCake(30);
+        BirthdayCake cake = new BirthdayCake(300000);
         System.out.println(cake.blowoutcake());
     }
 
